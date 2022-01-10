98c4affd5c94c35b7737cca6867cf5155231daf4
Preview <mr.arnob101@gmail.com>
2022-01-11 00:13:56 +0600

    update app

--- backend/controllers/Cloudinary.js
+++ backend/controllers/Cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-    cloud_name: "light-house",
-    api_key: "188565842427491",
-    api_secret: "TcaaLFJsy9CwG44AG38VQ10ZGOs"
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY
 })
 
 module.exports = cloudinary
